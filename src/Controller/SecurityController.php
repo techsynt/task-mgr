f84630dccb65f22adc83e502ac0e3e3aebdbb249
techsynt <techsynt@gmail.com>
2024-01-18 11:42:09 +0300

    installed translations, edited templates

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,19 +7,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
     public function login(Request $request): Response
     {
         $form = $this->createForm(LoginType::class);
-//        if ($request->getSession()->get('error')) {
-//            dd($request->getSession()->get('error'));
-//        }
+
+        $error = $request->getSession()->get('error');
+        $request->getSession()->clear();
 
         return $this->render('security/login.html.twig', [
             'form' => $form,
-            'error' => $request->getSession()->get('error'),
+            'error' => $error,
         ]);
     }
 }
