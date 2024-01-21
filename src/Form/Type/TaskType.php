19dcca3e42c5ecad45e0b0f643df0f65aea20a42
techsynt <techsynt@gmail.com>
2024-01-21 14:31:13 +0300

    added webpack encore

--- src/Form/Type/TaskType.php
+++ src/Form/Type/TaskType.php
@@ -4,7 +4,6 @@ namespace App\Form\Type;
 
 use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
