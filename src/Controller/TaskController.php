af8362f14fb252004b48450141d146ca25dd8b79
techsynt <techsynt@gmail.com>
2024-01-21 13:27:52 +0300

    creating add task page

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\Type\TaskType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TaskController extends AbstractController
 {
+    #[Route('/add', name: 'app_add_task')]
+    public function addTask(): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+        return $this->render('task/add_task.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
     #[Route('/', name: 'app_browse')]
     public function homepage(): Response
     {
---
+++ src/Form/Type/TaskType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => false,
+                'attr' => ['placeholder' => 'Заголовок', 'rows' => '5'],
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => false,
+                'attr' => ['placeholder' => 'Текст задачи'],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Добавить задачу',
+            ]);
+    }
+}
